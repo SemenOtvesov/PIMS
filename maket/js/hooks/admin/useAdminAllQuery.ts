8b2da9cd7e256519a713948c0fba8d1e9c7b274f
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2025-05-28 19:49:20 +0300

    allCheckRequest

--- maket/js/hooks/admin/useAdminAllQuery.ts
+++ maket/js/hooks/admin/useAdminAllQuery.ts
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
 import useAppSelector from '../useAppSelector';
-import getLocations from '@js/api/getLocations';
 import useAppDispatch from '../useAppDispatch';
+import getEmployee from '@js/api/getEmployee';
+import getAwards from '@js/api/getAwards';
+import getNews from '@js/api/getNews';
+import getLocationsCopy from '@js/api/getLocations copy';
 
 export default () => {
     const dispatch = useAppDispatch();
     const adminToken = useAppSelector(state => state.adminState.token);
 
     useEffect(() => {
-        getLocations(dispatch, adminToken);
+        getLocationsCopy(dispatch, adminToken);
+        getEmployee(dispatch, adminToken);
+        getAwards(dispatch, adminToken);
+        getNews(dispatch, adminToken);
     }, [adminToken]);
 };
